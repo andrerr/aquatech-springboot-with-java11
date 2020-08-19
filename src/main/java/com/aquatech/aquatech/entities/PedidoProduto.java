48932fe5ab22f1257b82bfd0a80aa89d823dcc13
andrerr <andreribeiro13@hotmail.com>
2020-08-18 21:52:05 -0300

    Classe associativa para pedidoxproduto

---
+++ src/main/java/com/aquatech/aquatech/entities/PedidoProduto.java
@@ -0,0 +1,87 @@
+package com.aquatech.aquatech.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.aquatech.aquatech.entities.pk.PedidoProdutoPK;
+
+@Entity
+@Table(name = "tb_pedido_produto")
+public class PedidoProduto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private PedidoProdutoPK id;
+	
+	private Integer quantidade;
+	private Double preco;
+	
+	public PedidoProduto(Pedido pedido, Produto produto, Integer quantidade, Double preco) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+	
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PedidoProduto other = (PedidoProduto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
